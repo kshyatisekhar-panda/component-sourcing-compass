37acfa30891acdcfa9d408fc2a08a9095da0ddad
thee <theekshana.sandaru@gmail.com>
2026-05-08 23:26:29 +0200

    removingn key

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,5 @@
-export const MOUSER_API_KEY =
-  process.env.MOUSER_API_KEY ?? "90a635a0-454e-478c-8351-52fd8f454300";
-
-export const TAVILY_API_KEY =
-  process.env.TAVILY_API_KEY ?? "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";
+export const MOUSER_API_KEY = process.env.MOUSER_API_KEY ?? "";
+export const TAVILY_API_KEY = process.env.TAVILY_API_KEY ?? "";
 
 export const MOUSER_BASE_URL = "https://api.mouser.com/api/v1";
 export const TAVILY_BASE_URL = "https://api.tavily.com";
